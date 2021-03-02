0b6bf691afb7c13b5b0ec721b1457bc4e2a57097
Mohammad Raza <qarar@Razas-MacBook-Air.local>
2021-03-02 20:18:20 +0530

    Adding sets

--- Intro/intro.java
+++ Intro/intro.java
@@ -11,32 +11,32 @@ public class intro {
 		
 	
 		
-		int n = sc.nextInt();
-		
-		int k = sc.nextInt();
-		
-		int arr[] = new int[n];
-		
-		for(int i = 0; i<n;i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i =0; i<n; i++){
-                 if(i < k){
-             pq.add(arr[i]);}
-           else{
-               if(pq.peek() < arr[i]){
-                   pq.poll();
-                   pq.add(arr[i]);}
-               
-              }
-          }
-      ArrayList<Integer> ans = new ArrayList<>(pq);
-      Collections.sort(ans, Collections.reverseOrder());
-      for(int a : ans)
-          System.out.println(a+ " ");} 
-        
+//		int n = sc.nextInt();
+//		
+//		int k = sc.nextInt();
+//		
+//		int arr[] = new int[n];
+//		
+//		for(int i = 0; i<n;i++) {
+//			arr[i] = sc.nextInt();
+//		}
+//		
+//		PriorityQueue<Integer> pq = new PriorityQueue<>();
+//        for(int i =0; i<n; i++){
+//                 if(i < k){
+//             pq.add(arr[i]);}
+//           else{
+//               if(pq.peek() < arr[i]){
+//                   pq.poll();
+//                   pq.add(arr[i]);}
+//               
+//              }
+//          }
+//      ArrayList<Integer> ans = new ArrayList<>(pq);
+//      Collections.sort(ans, Collections.reverseOrder());
+//      for(int a : ans)
+//          System.out.println(a+ " ");} 
+//        
 //          int a[] = new int[k];
 //          for(int i = k-1; i>=0; i--){
 //          a[i] = pq.poll();
@@ -60,12 +60,39 @@ public class intro {
 //			
 //		 }else 
 //			 System.out.println("Odd");
+		
+		System.out.print("Enter size of array: ");
+		int n = sc.nextInt();
 	
+		int a[]=  new int[n];
+		if(n==0) {
+			System.out.println(n);}
+			else {	
+		System.out.println("Enter non negative integer values for array: ");
+		
+		for(int i =0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		
+		
+		}
+		
+		Deque<Integer> dq = new ArrayDeque<>();
+		
+		for(int i = 0; i<n;i++) {
+			if(a[i]%2 == 0) {
+				dq.addFirst(a[i]);
+			}else {
+				dq.addLast(a[i]);
+			}
 		
 	 
+		
+			System.out.println(dq);}}
+		
  }
  
- 
+
  
 
 		 
